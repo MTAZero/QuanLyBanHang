c4728af012b3d71a2c821a678efc75d6c2709b2f
windatm <tuanphongmta0205@gmail.com>
2017-09-25 15:50:42 +0700

    FrmNhapHang 1

--- QLBanHang/QLBanHang/GUI/FrmNhapHang.cs
+++ QLBanHang/QLBanHang/GUI/FrmNhapHang.cs
@@ -13,7 +13,28 @@ namespace QLBanHang.GUI
 {
     public partial class FrmNhapHang : Form
     {
-       
+        private QLBanHangDbContext db = Service.DBService.db;
+        private int indexPhieuNhap = 0, indexPhieuNhap1 = 0;
+        private int indexChiTietNhap = 0, indexChiTietNhap1 = 0;
+        private NHANVIEN nv = new NHANVIEN();
+
+        #region constructor
+        public FrmNhapHang(NHANVIEN _nv)
+        {
+            InitializeComponent();
+            Service.DBService.Reload();
+            nv = _nv;
+        }
+        #endregion
+
+        #region Form chính
+        private void FrmNhapHang_Load(object sender, EventArgs e)
+        {
+            LoadPhieuNhap();
+            LoadChiTietNhap();
+        }
+        #endregion
+
 
     }
 }
