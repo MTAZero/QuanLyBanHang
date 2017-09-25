1c2995c2e99da84b4e000faf13c2e876948eefea
MTAZero <buithuy3996@gmail.com>
2017-09-25 13:43:13 +0700

    From Quan Ly Mat Hang lan 1

--- QLBanHang/QLBanHang/GUI/FrmQuanLyMatHang.cs
+++ QLBanHang/QLBanHang/GUI/FrmQuanLyMatHang.cs
@@ -13,6 +13,58 @@ namespace QLBanHang.GUI
 {
     public partial class FrmQuanLyMatHang : Form
     {
-        
+        private QLBanHangDbContext db = Service.DBService.db;
+        private int index = 0, index1 = 0;
+
+        #region constructor
+        public FrmQuanLyMatHang()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region LoadForm
+
+        private void LoadControl()
+        {
+            groupThongTin.Enabled = false;
+        }
+
+        private void LoadDgvNhanVien()
+        {
+            int i = 0;
+            string keyword = txtTimKiem.Text;
+            var dbNV = db.MATHANGs.ToList()
+                       .Select(p => new
+                       {
+                           ID = p.ID,
+                           STT = ++i,
+                           TenMH = p.TEN,
+                           DonViTinh = p.DONVITINH,
+                           GhiChu = p.GHICHU
+                       })
+                       .ToList();
+
+            dgvMatHang.DataSource = dbNV
+                                    .Where(p => p.TenMH.Contains(keyword) || p.DonViTinh.Contains(keyword))
+                                    .ToList();
+
+            // cập nhật index 
+            index = index1;
+            try
+            {
+                dgvMatHang.Rows[index].Cells["STT"].Selected = true;
+                dgvMatHang.Select();
+            }
+            catch { }
+        }
+
+
+        private void FrmQuanLyNhanVien_Load(object sender, EventArgs e)
+        {
+            LoadControl();
+            LoadDgvNhanVien();
+        }
+        #endregion
     }
 }
